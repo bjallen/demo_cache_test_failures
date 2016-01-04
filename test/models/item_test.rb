8dcf4c70000673e2c3cd7e6cc4e266a3fd52c9fe
B.J. Allen <ballen@numerex.com>
2016-01-04 10:22:16 -0600

    add tests that pass with no cache

--- test/models/item_test.rb
+++ test/models/item_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class ItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'factories' do
+    name = "Factory Item"
+    item = FactoryGirl.build_stubbed(:item, name: name)
+    assert_equal name, item.name
+  end
+
+  test 'database' do
+    1001.times do
+      name = "Database Item"
+      item = Item.create(name: name)
+      assert_equal name, item.name
+    end
+  end
 end
