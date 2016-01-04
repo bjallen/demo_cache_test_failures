f7e439dc90148cf0b6e531c65ffca7161fe9b961
B.J. Allen <ballen@numerex.com>
2016-01-04 10:33:21 -0600

    use cache_key instead of id for item name cache
    This fixes the test failure on master. With this, you can also remove
    the hard-coded expiration since the cache_key uses the updated_at by
    default.

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ActiveRecord::Base
   def name
-    Rails.cache.fetch("item_name_#{id}", expires_in: 10.seconds) do
+    Rails.cache.fetch("item_name_#{cache_key}") do
       read_attribute(:name)
     end
   end
